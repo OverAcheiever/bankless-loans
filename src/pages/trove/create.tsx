8dda440ba8cc3192fec29cbdc054dc107e5945b0
Birdman <geositta@protonmail.com>
2021-10-27 21:07:31 -0500

    Remove stepper UI and sidebar

--- src/pages/trove/create.tsx
+++ src/pages/trove/create.tsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import { Box, Stack, Heading, Flex, Input, Button } from '@chakra-ui/react'
 import StyledBox from '../../components/StyledBox'
 import TroveProvider, { TroveContext } from './TroveContext'
-import Stepper from '../../components/Steps'
 
 const TroveHeader = () => {
   return (
@@ -14,7 +13,6 @@ const TroveHeader = () => {
           alignContent='flex-start'
           >
           <div>Borrow LUSD</div>
-          <Stepper />
         </Flex>
     </StyledBox>
   )
@@ -36,63 +34,6 @@ const TroveForm = () => {
       <TroveHeader />
       <div>hi, i am a form</div>
     </StyledBox>
-  )
-}
-
-const TroveSidebar = () => {
-  const {loan, addLoan } = useContext(TroveContext)
-
-  return (
-    <Flex
-      maxHeight="calc(100vh - 305px)"
-      height='auto'
-      display="flex"
-      flexDirection="column"
-      justifyContent="space-around"
-      w={["100%", "25%"]}
-      bg="auto"
-    >
-      <StyledBox bg="white" variant='section'
-        borderRadius='18px'>
-        <Flex flexDirection='row'
-          color='black'
-          justifyContent="space-between"
-          alignContent='flex-start'
-          >
-          <div>Liquidation Risk</div>
-          <div>LOW</div>
-        </Flex>
-        <Flex color='black'>
-          The risk that your Trove may become subject to liquidation,
-          taking into account the extra risk of liquidation during recovery mode.
-          Learn More
-        </Flex>
-      </StyledBox>
-      <StyledBox bg="white" color='black'
-        borderRadius='18px'>
-        <Flex flexDirection='row'
-          color='black'
-          justifyContent="space-between"
-          alignContent='flex-start'
-          >
-          <div>Current Collateralization ratio</div>
-          <div>160%</div>
-        </Flex>
-      </StyledBox>
-      <StyledBox bg="white" color='black'
-        borderRadius='18px'>
-        <Flex flexDirection='row'
-          color='black'
-          justifyContent="space-between"
-          alignContent='flex-start'
-          >
-          <div>Redemption Risk</div>
-          <div>HIGH</div>
-        </Flex>
-        <div>The risk that your Trove may be repaid by redeemers</div>
-        <div>Learn more</div>
-      </StyledBox>
-    </Flex>
   )
 }
 
@@ -131,7 +72,6 @@ const TroveCreate = () => (
       direction="row"
     >
       <TroveForm />
-      <TroveSidebar />
     </Flex>
   </TroveProvider>
 )
